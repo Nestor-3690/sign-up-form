886fc8988619a646285f25d51abb6450d7bde9f5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-20 15:56:38 +0200

    Add logic to hidden span when pwds (don't) match

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const password = document.querySelector("input[name=password]")
-const confirm = document.querySelector("input[name=confirm-password]")
+const password = document.querySelector("input[name=password]");
+const confirm = document.querySelector("input[name=confirm-password]");
+const span = document.querySelector("span");
 
 
 password.addEventListener("change", () => {
     if (password.value === confirm.value) {
         password.style.border = "1px solid #E5E7EB";
+        span.style.visibility = "hidden";
     } else if (confirm.value === "") {
             password.style.border = "1px solid #E5E7EB";
     } else {
         password.style.border = "1px solid red";
         confirm.style.border = "1px solid red";
+        span.style.visibility = "visible";
     }
 })
 
@@ -17,8 +20,10 @@ confirm.addEventListener("change", () => {
     if (password.value === confirm.value) {
         password.style.border = "1px solid #E5E7EB";
         confirm.style.border = "1px solid #E5E7EB";
+        span.style.visibility = "hidden";
     } else {
         password.style.border = "1px solid red";
         confirm.style.border = "1px solid red";
+        span.style.visibility = "visible";
     }
 })
